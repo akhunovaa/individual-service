f82c0ba2f135c96100efd29dbf42342799842e9e
Azat Akhunov <azat.akhunov@protonmail.com>
2020-02-13 21:33:39 +0300

    [CF] picture upload controller updated

--- src/main/java/com/botmasterzzz/individual/controller/IndividualResourceController.java
+++ src/main/java/com/botmasterzzz/individual/controller/IndividualResourceController.java
@@ -81,7 +81,7 @@ public class IndividualResourceController extends AbstractController {
     public Response userImageUpload(@RequestParam("file") MultipartFile file) {
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
         UserPrincipal userPrincipal = (UserPrincipal) usernamePasswordAuthenticationToken.getPrincipal();
-        String requestUrl = "https://yourapi.ru/individual";
+        String requestUrl = "https://dev.yourapi.ru/individual";
         imageValidatorService.validate(file);
         storageService.storeMainImage(file, userPrincipal.getId());
         String imageUrl = requestUrl + "/image/" + userPrincipal.getId();

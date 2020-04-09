b7f5d3023411a40c7f98f04ca05df42b0b155a6f
Azat Akhunov <azat.akhunov@protonmail.com>
2020-04-09 23:47:43 +0300

    [NF] auth controller for user info has been purged

--- src/main/java/com/botmasterzzz/individual/controller/UserProfileController.java
+++ src/main/java/com/botmasterzzz/individual/controller/UserProfileController.java
@@ -1,17 +1,19 @@
 package com.botmasterzzz.individual.controller;
 
-import com.botmasterzzz.individual.dto.UserPrincipal;
 import com.botmasterzzz.individual.dto.UserProfileDTO;
 import com.botmasterzzz.individual.model.Response;
 import com.botmasterzzz.individual.service.UserService;
+import com.botmasterzzz.individual.util.ClientInfoUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class UserProfileController extends AbstractController {
@@ -22,13 +24,17 @@ public class UserProfileController extends AbstractController {
     private UserService userService;
 
     @RequestMapping(value = "{nickname}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("authenticated")
-    public Response userInfo(@PathVariable(name = "nickname") String nickname) {
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        UserPrincipal userPrincipal = (UserPrincipal) usernamePasswordAuthenticationToken.getPrincipal();
-        LOGGER.info("User info invoking request came from login: {} and id: {} requested username: {}", userPrincipal.getLogin(), userPrincipal.getId(), nickname);
+    public Response userInfo(@PathVariable(name = "nickname") String nickname, HttpServletRequest httpServletRequest) {
+        String clientBrowser = ClientInfoUtil.getClientBrowser(httpServletRequest);
+        String clientOs = ClientInfoUtil.getClientOS(httpServletRequest);
+        String clientIp = ClientInfoUtil.getClientIpAddr(httpServletRequest);
+        String xRealIp = httpServletRequest.getHeader("X-Real-IP");
+        String xForwardedFor = httpServletRequest.getHeader("X-Forwarded-For");
+        String host = httpServletRequest.getHeader("Host");
+        String xForwardedProto = httpServletRequest.getHeader("X-Forwarded-Proto");
+        LOGGER.info("User info invoking request came for nickname: {} \n{} \n {} \n {} \n {} \n {} \n {} \n {}", nickname, clientBrowser, clientOs, clientIp, xRealIp, xForwardedFor, host, xForwardedProto);
         UserProfileDTO userProfileDTO = userService.findUserProfile(nickname);
-        LOGGER.info("User info data came with a name: {} and id: {} requested username: {}", userProfileDTO.getName(), userProfileDTO.getId(), nickname);
+        LOGGER.info("User info invoking request done for nickname: {} \n {} \n{} \n {} \n {} \n {} \n {} \n {} \n {}", nickname, userProfileDTO, clientBrowser, clientOs, clientIp, xRealIp, xForwardedFor, host, xForwardedProto);
         return getResponseDto(userProfileDTO);
     }
 
